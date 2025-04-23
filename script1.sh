f10ced1d6bfdf811f1bc8f35e23c97a33aac2312
398103481wdawd <mail@cruizernw.com.tr>
2025-04-23 20:20:32 +0300

    Update and rename script.sh to script1.sh

---
+++ script1.sh
@@ -0,0 +1,25 @@
+echo "FROM debian:12
+
+# Update package lists
+RUN apt-get update
+
+# Install systemd and systemd-sysv, and set root password
+RUN apt-get install -y systemd systemd-sysv
+
+# Set the root password
+RUN echo \"root:root\" | chpasswd
+
+# Set environment variable to indicate running in Docker
+ENV container=docker
+
+# Define the command to run when the container starts
+CMD [\"/sbin/init\"]
+
+# Expose SSH and your application port
+EXPOSE 22 8006
+
+# Define volumes (these need to be mounted at runtime with docker run -v)
+VOLUME /sys/fs/cgroup
+VOLUME /proc
+VOLUME /dev
+VOLUME /run/systemd" > Dockerfile
